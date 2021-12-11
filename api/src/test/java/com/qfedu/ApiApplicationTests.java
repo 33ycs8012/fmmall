6854843edc1d5128436fe1cc32be52f4208cc4ef
yangchunsheng <2206396952@qq.com>
2021-12-11 20:15:35 +0800

    第二次提交

--- api/src/test/java/com/qfedu/ApiApplicationTests.java
+++ api/src/test/java/com/qfedu/ApiApplicationTests.java
@@ -1,13 +1,25 @@
 package com.qfedu;
 
+import com.qfedu.fmmall.dao.TestUserDAO;
+import com.qfedu.fmmall.entity.TestUser;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class ApiApplicationTests {
 
+    @Resource
+    TestUserDAO testUserDAO;
+
     @Test
     void contextLoads() {
+        TestUser testUser = testUserDAO.queryUserByName("aa");
+        System.out.println("testUser = " + testUser);
+
     }
+
+
 
 }
