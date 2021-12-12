332a3d1801aa0301135bffcbae801fc589892c96
yangchunsheng <2206396952@qq.com>
2021-12-12 16:53:43 +0800

    整合Swagger

--- api/src/main/java/com/qfedu/fmmall/controller/UserController.java
+++ api/src/main/java/com/qfedu/fmmall/controller/UserController.java
@@ -3,10 +3,7 @@ package com.qfedu.fmmall.controller;
 import com.qfedu.fmmall.service.UserService;
 import com.qfedu.fmmall.vo.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -14,7 +11,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/login")
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
     public ResultVO userLogin(@RequestParam String name,@RequestParam String real){
         ResultVO resultVO = userService.checkLogin(name, real);
         return resultVO;
